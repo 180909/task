b45b7a50ec2bfcf9e094a4dd129637890d17ce09
180909 <734461790@qq.com>
2021-08-22 23:44:41 +0800

    Test time.Timer

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	fmt.Println(time.Now().String())
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println(<-timer.C)
 }
